d669569b5dfdefabd3a3fa47c9c61b1dde7708f6
oreosoul <654181494@qq.com>
2016-07-13 18:10:03 +0800

    day2 afternoon
    Signed-off-by: oreosoul <654181494@qq.com>

---
+++ js/data.js
@@ -0,0 +1,35 @@
+var users = [
+	{name: "CHEUK",
+		id:null,
+		flag:1
+	},
+	{name: "浴巾",
+		id:null,
+		flag:2
+	},
+	{name: "YOKI",
+		id:null,
+		flag:3
+	},
+	{name: "TIMO",
+		id:null,
+		flag:3
+	},
+	{name: "123",
+		id:null,
+		flag:0
+	}
+];
+var rooms = [
+	{   
+		name: "来战"
+	},{   
+		name: "不怂"
+	},{   
+		name: "浴巾快来"
+	},{   
+		name: "YOKI来战"
+	},{   
+		name: "来战"
+	}
+];
